62f9429cf0d0a7df9665dceef583391996cf4b2e
enescang <developerindiee@gmail.com>
2023-07-20 22:29:08 +0300

    IterationResult and OutputResult structures added

--- mlp/input.go
+++ mlp/input.go
@@ -4,3 +4,14 @@ type InputRow struct {
 	Inputs    []float64
 	Expecteds []float64
 }
+
+type IterationResult struct {
+	Passed  bool
+	Outputs []OutputResult
+}
+
+type OutputResult struct {
+	Output   float64
+	Expected float64
+	Error    float64
+}
